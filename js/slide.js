e7a9f8ac75998894e23c33c8ce0b7d05ea12006c
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-02-26 19:58:36 -0300

    banner slide animation completed

--- js/slide.js
+++ js/slide.js
@@ -1,17 +1,50 @@
 var swiper = new Swiper(".products-slider", {
-    loop:true,
-    spaceBetween: 20,
-    grabCursor:true,
-    centeredSlides:true,
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      991: {
-        slidesPerView: 3,
-      },
+  loop: true,
+  spaceBetween: 20,
+  grabCursor: true,
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-  });
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+//slider produtos
+let productPreviewContainer = document.querySelector(".products-preview-container");
+let productPreview = document.querySelectorAll(".products-preview-container .product-preview");
+let pageProductSelect = document.querySelectorAll(".products .slide .btn");
+let closeButtonSelect = document.querySelectorAll(".products-preview-container .product-preview .fa-times");
+
+pageProductSelect.forEach((detailBtn) => {
+  detailBtn.onclick = () => {
+    productPreviewContainer.style.display = "block";
+    let name = detailBtn.getAttribute("data-product");
+
+    productPreview.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+
+      if (name == target) {
+        preview.style.display = "flex";
+        return true;
+      } else {
+        preview.style.display = "none";
+      }
+    });
+  };
+});
+
+//Fechar botão do produto
+closeButtonSelect.forEach((close) => {
+  close.onclick = () => {
+    productPreviewContainer.style.display = "none";
+  };
+});
